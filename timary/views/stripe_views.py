c240ffff443e04a2b2df911ccc3faa1c1a3b4d0c
Aristotel Fani <afani1997@gmail.com>
2022-01-02 17:09:48 -0500

    Sent text to user when invoice has been paid

--- timary/views/stripe_views.py
+++ timary/views/stripe_views.py
@@ -1,5 +1,7 @@
+from django.conf import settings
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
+from twilio.rest import Client
 
 from timary.models import SentInvoice
 
@@ -10,4 +12,12 @@ def invoice_payment_success(request, invoice_id):
         return redirect(reverse("timary:login"))
     sent_invoice.paid_status = SentInvoice.PaidStatus.PAID
     sent_invoice.save()
+    if sent_invoice.invoice.user.phone_number:
+        client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
+        _ = client.messages.create(
+            to=sent_invoice.invoice.user.formatted_phone_number,
+            from_=settings.TWILIO_PHONE_NUMBER,
+            body=f"Invoice for {sent_invoice.invoice.title} has been paid! "
+            f"You should see {sent_invoice.total_price} deposited into your bank account shortly",
+        )
     return render(request, "invoices/success_pay_invoice.html", {})
