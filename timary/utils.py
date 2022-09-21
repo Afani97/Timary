ceca78c570f2136cc0683d9d59818faaa984b9ce
Aristotel Fani <afani1997@gmail.com>
2022-09-21 16:10:52 -0400

    add util helper method to decrease redundant lines of code

--- timary/utils.py
+++ timary/utils.py
@@ -1,5 +1,7 @@
 import json
 
+from django.shortcuts import render
+
 
 def show_alert_message(response, alert_type, message, other_trigger=None):
     response["HX-Trigger"] = json.dumps(
@@ -10,4 +12,13 @@ def show_alert_message(response, alert_type, message, other_trigger=None):
                 "message": message,
             },
         }
+    )
+
+
+def render_xml(r, t, c=None):
+    return render(
+        r,
+        f"mobile/{t}",
+        context=c,
+        content_type="application/xml",
     )
--- timary/views/mobile.py
+++ timary/views/mobile.py
@@ -1,5 +1,5 @@
 from django.http import Http404
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.decorators import (
     api_view,
@@ -14,11 +14,12 @@ from rest_framework_xml.renderers import XMLRenderer
 
 from timary.forms import DailyHoursForm, UserForm
 from timary.models import DailyHoursInput
+from timary.utils import render_xml
 from timary.views import get_hours_tracked
 
 
 def mobile_index(request):
-    return render(request, "mobile/index.xml", content_type="application/xml")
+    return render_xml(request, "index.xml")
 
 
 @api_view(["GET"])
@@ -35,18 +36,9 @@ def mobile_hours(request):
     }
     context.update(get_hours_tracked(request.user))
     if "partial" in request.query_params:
-        return render(
-            request,
-            "mobile/hours/_hours.xml",
-            context=context,
-            content_type="application/xml",
-        )
-    return render(
-        request,
-        "mobile/hours/hours.xml",
-        context=context,
-        content_type="application/xml",
-    )
+        return render_xml(request, "hours/_hours.xml", context)
+    else:
+        return render_xml(request, "hours/hours.xml", context)
 
 
 @api_view(["GET", "POST"])
@@ -59,26 +51,18 @@ def mobile_new_hours(request):
         hours = DailyHoursForm(request.data)
         if hours.is_valid():
             hours.save()
-            return render(
-                request,
-                "mobile/new-hours/new_hours_form.xml",
-                {"success": True},
-                content_type="application/xml",
+            return render_xml(
+                request, "new-hours/new_hours_form.xml", {"success": True}
             )
         else:
-            return render(
-                request,
-                "mobile/new-hours/new_hours_form.xml",
-                {"errors": hours.errors},
-                content_type="application/xml",
+            return render_xml(
+                request, "new-hours/new_hours_form.xml", {"errors": hours.errors}
             )
     else:
-        context = {"user_invoices": request.user.get_invoices}
-        return render(
+        return render_xml(
             request,
-            "mobile/new-hours/new_hours.xml",
-            context=context,
-            content_type="application/xml",
+            "new-hours/new_hours.xml",
+            {"user_invoices": request.user.get_invoices},
         )
 
 
@@ -89,26 +73,14 @@ def mobile_view_hours(request, hours_id):
     hour = get_object_or_404(DailyHoursInput, id=hours_id)
     if request.user != hour.invoice.user:
         raise Http404
-    return render(
-        request,
-        "mobile/hours/_hour.xml",
-        context={"hour": hour},
-        content_type="application/xml",
-    )
+    return render_xml(request, "hours/_hour.xml", {"hour": hour})
 
 
 @api_view(["GET"])
 @authentication_classes([TokenAuthentication])
 @permission_classes([IsAuthenticated])
 def mobile_hour_stats(request):
-    context = {}
-    context.update(get_hours_tracked(request.user))
-    return render(
-        request,
-        "mobile/hours/_stats.xml",
-        context=context,
-        content_type="application/xml",
-    )
+    return render_xml(request, "hours/_stats.xml", get_hours_tracked(request.user))
 
 
 @api_view(["GET", "POST"])
@@ -124,26 +96,15 @@ def mobile_edit_hours(request, hours_id):
         hours = DailyHoursForm(request.data, instance=hour)
         if hours.is_valid():
             hours.save()
-            return render(
-                request,
-                "mobile/edit-hours/edit_hours_form.xml",
-                {"hour": hour, "success": True},
-                content_type="application/xml",
-            )
+            context = {"hour": hour, "success": True}
         else:
-            return render(
-                request,
-                "mobile/edit-hours/edit_hours_form.xml",
-                {"hour": hour, "errors": hours.errors},
-                content_type="application/xml",
-            )
+            context = {"hour": hour, "errors": hours.errors}
+        return render_xml(request, "edit-hours/edit_hours_form.xml", context)
     else:
-        context = {"hour": hour, "user_invoices": request.user.get_invoices}
-        return render(
+        return render_xml(
             request,
-            "mobile/edit-hours/edit_hours.xml",
-            context=context,
-            content_type="application/xml",
+            "edit-hours/edit_hours.xml",
+            {"hour": hour, "user_invoices": request.user.get_invoices},
         )
 
 
@@ -155,30 +116,17 @@ def mobile_delete_hours(request, hours_id):
     if request.user != hours.invoice.user:
         raise Http404
     hours.delete()
-    return render(
-        request,
-        "mobile/empty.xml",
-        content_type="application/xml",
-    )
+    return render_xml(request, "empty.xml")
 
 
 @api_view(["GET"])
 @authentication_classes([TokenAuthentication])
 @permission_classes([IsAuthenticated])
 def mobile_profile(request):
+    t = "profile/profile.xml"
     if "partial" in request.query_params:
-        return render(
-            request,
-            "mobile/profile/_profile.xml",
-            {"profile": request.user},
-            content_type="application/xml",
-        )
-    return render(
-        request,
-        "mobile/profile/profile.xml",
-        {"profile": request.user},
-        content_type="application/xml",
-    )
+        t = "profile/_profile.xml"
+    return render_xml(request, t, {"profile": request.user})
 
 
 @api_view(["GET", "POST"])
@@ -191,23 +139,11 @@ def mobile_edit_profile(request):
         profile = UserForm(request.data, instance=request.user)
         if profile.is_valid():
             profile.save()
-            return render(
-                request,
-                "mobile/edit-profile/edit_profile_form.xml",
-                {"profile": request.user, "success": True},
-                content_type="application/xml",
-            )
+            context = {"profile": request.user, "success": True}
         else:
-            return render(
-                request,
-                "mobile/edit-profile/edit_profile_form.xml",
-                {"profile": request.user, "errors": profile.errors},
-                content_type="application/xml",
-            )
+            context = {"profile": request.user, "errors": profile.errors}
+        return render_xml(request, "edit-profile/edit_profile_form.xml", context)
     else:
-        return render(
-            request,
-            "mobile/edit-profile/edit_profile.xml",
-            {"profile": request.user},
-            content_type="application/xml",
+        return render_xml(
+            request, "edit-profile/edit_profile.xml", {"profile": request.user}
         )
