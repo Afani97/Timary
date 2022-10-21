e81aa99c39ce1f4c119a4105087fd4a3f5fd796d
Aristotel Fani <afani1997@gmail.com>
2022-10-21 19:17:42 -0400

    use django-twilio client instead in service

--- timary/services/twilio_service.py
+++ timary/services/twilio_service.py
@@ -1,19 +1,14 @@
 import datetime
 
 from django.conf import settings
-from twilio.rest import Client
+from django_twilio.client import twilio_client
 
 
 class TwilioClient:
     @staticmethod
-    def client():
-        client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
-        return client
-
-    @staticmethod
     def sent_payment_success(sent_invoice):
         if sent_invoice.invoice.user.phone_number:
-            _ = TwilioClient.client().messages.create(
+            _ = twilio_client.messages.create(
                 to=sent_invoice.invoice.user.formatted_phone_number,
                 from_=settings.TWILIO_PHONE_NUMBER,
                 body=f"Invoice for {sent_invoice.invoice.title} has been paid! "
@@ -22,7 +17,7 @@ class TwilioClient:
 
     @staticmethod
     def log_hours(invoice):
-        _ = TwilioClient.client().messages.create(
+        _ = twilio_client.messages.create(
             to=invoice.user.formatted_phone_number,
             from_=settings.TWILIO_PHONE_NUMBER,
             body=f"How many hours to log for: {invoice.title}. Reply 'S' to skip",
@@ -30,7 +25,7 @@ class TwilioClient:
 
     @staticmethod
     def send_message(user, message):
-        _ = TwilioClient.client().messages.create(
+        _ = twilio_client.messages.create(
             to=user.formatted_phone_number,
             from_=settings.TWILIO_PHONE_NUMBER,
             body=message,
@@ -38,7 +33,7 @@ class TwilioClient:
 
     @staticmethod
     def get_user_messages(user_number):
-        recent_messages = TwilioClient.client().messages.list(
+        recent_messages = twilio_client.messages.list(
             to=user_number, limit=1, date_sent=datetime
         )
         if recent_messages and len(recent_messages) == 1:
