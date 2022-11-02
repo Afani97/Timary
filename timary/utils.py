74677f5d4d740f5250b96ee067b3c70952a9bb39
Aristotel Fani <afani1997@gmail.com>
2022-11-02 12:54:57 -0400

    allow alerts to persist

--- timary/utils.py
+++ timary/utils.py
@@ -1,13 +1,16 @@
 import json
 
 
-def show_alert_message(response, alert_type, message, other_trigger=None):
+def show_alert_message(
+    response, alert_type, message, other_trigger=None, persist=False
+):
     response["HX-Trigger"] = json.dumps(
         {
             other_trigger: None,
             "showMessage": {
                 "alertType": f"alert-{alert_type}",
                 "message": message,
+                "persist": persist,
             },
         }
     )
