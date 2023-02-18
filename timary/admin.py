3be2c394f3ee0479f8a1badf153619e77ebe9aff
Aristotel Fani <afani1997@gmail.com>
2023-02-18 17:05:39 -0500

    cleanup admin

--- timary/admin.py
+++ timary/admin.py
@@ -1,5 +1,3 @@
-import datetime
-
 from django import forms
 from django.contrib import admin, messages
 from django.db.models import Sum
@@ -32,14 +30,25 @@ admin.site.register(HoursLineItem)
 
 
 class SendEmailForm(forms.Form):
-    subject = forms.CharField(max_length=300)
-    message = forms.CharField(max_length=2000, widget=forms.Textarea)
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields["message"].widget.attrs["rows"] = 8
-        self.fields["message"].widget.attrs["cols"] = 15
-        self.fields["message"].widget.attrs["textarea"] = True
+    subject = forms.CharField(
+        max_length=300,
+        widget=forms.Textarea(
+            attrs={
+                "rows": 1,
+                "class": "textarea textarea-bordered border-2 text-lg w-full",
+            }
+        ),
+    )
+    message = forms.CharField(
+        max_length=2000,
+        widget=forms.Textarea(
+            attrs={
+                "rows": 8,
+                "cols": 15,
+                "class": "textarea textarea-bordered border-2 text-lg w-full",
+            }
+        ),
+    )
 
 
 def send_emails(subject, message):
@@ -52,7 +61,9 @@ class TimaryAdminSite(OTPAdminSite):
     index_template = "admin/custom_index.html"
 
     def analytics(self, request):
-        users = {"total": User.objects.count()}
+        users = {
+            "total": int(User.objects.exclude(stripe_subscription_status=3).count())
+        }
         sent_invoices = {
             "total": SentInvoice.objects.count(),
             "pending": SentInvoice.objects.filter(
@@ -72,21 +83,10 @@ class TimaryAdminSite(OTPAdminSite):
             "invoice_total": Invoice.objects.count(),
             "contracts_total": Contract.objects.count(),
         }
-        current_date = datetime.datetime.today()
-        sent_invoice_fees = SentInvoice.objects.filter(
-            date_sent__month__gte=current_date.month,
-            date_sent__year__gte=current_date.year,
-        ).aggregate(total=Sum("total_price"))
         money_stats = {
             "recurring": int(users["total"]) * 29,
         }
-        if sent_invoice_fees["total"] is not None:
-            money_stats["fees"] = sent_invoice_fees["total"] * 0.01
-        else:
-            money_stats["fees"] = 0
-        money_stats["mrr_goal"] = (
-            money_stats["recurring"] + money_stats["fees"]
-        ) / 10_000
+        money_stats["mrr_goal"] = (money_stats["recurring"]) / 10_000
         context = {
             "text": "Hello World",
             "users": users,
