908abc3b30ad567bc9722e35660e8c8084b9787f
Aristotel Fani <afani1997@gmail.com>
2023-03-11 11:51:27 -0500

    fix migrations

--- timary/migrations/0050_sentinvoice_email_id.py
+++ timary/migrations/0050_sentinvoice_email_id.py
@@ -6,10 +6,14 @@ from django.db import migrations, models
 import timary.models
 
 
+def create_new_ref_number():
+    return str(random.randint(1000000000, 9999999999))
+
+
 def assign_unique_email_id_to_sent_invoices(apps, schema_editor):
     SentInvoice = apps.get_model("timary", "SentInvoice")
     for sent_invoice in SentInvoice.objects.all():
-        sent_invoice.email_id = str(random.randint(1000000000, 9999999999))
+        sent_invoice.email_id = create_new_ref_number()
         sent_invoice.save()
 
 
@@ -23,11 +27,18 @@ class Migration(migrations.Migration):
             model_name="sentinvoice",
             name="email_id",
             field=models.CharField(
-                default=timary.models.create_new_ref_number, max_length=10, unique=True
+                default=timary.models.create_new_ref_number, max_length=10
             ),
         ),
         migrations.RunPython(
             code=assign_unique_email_id_to_sent_invoices,
-            reverse_code=migrations.operations.special.RunPython.noop,
+            reverse_code=migrations.RunPython.noop,
+        ),
+        migrations.AlterField(
+            model_name="sentinvoice",
+            name="email_id",
+            field=models.CharField(
+                default=timary.models.create_new_ref_number, max_length=10, unique=True
+            ),
         ),
     ]
